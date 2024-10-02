4855903e41bdabbc06e245ffcdf82a2ff5f7fe08
liliana serrano <lilianaserrano018@gmail.com>
2024-10-02 10:48:56 -0600

    ultima actualización

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const headerHeight = header.offsetHeight;
 // Función que cambia el color de fondo del menú cuando se desplaza
 window.addEventListener("scroll", () => {
     if (window.scrollY > headerHeight) {
-        menu.style.backgroundColor = "rgb(50,43,150)"; // Cambia el color de fondo a azul
+        menu.style.backgroundColor = "rgb(253, 162, 243)"; // Cambia el color de fondo a rosado
     } else {
         menu.style.backgroundColor = "rgba(0, 0, 0, 0)"; // Vuelve al fondo transparente
     }
